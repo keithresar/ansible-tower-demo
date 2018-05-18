a90cfba6a77851f85c1ebc79fcaa6f51e3252abe
Keith Resar <keith.resar@422long.com>
2018-05-18 10:29:17 -0500

    -

--- tower_survey.sh
+++ tower_survey.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+JOB_TEMPLATE="25"
+
+
+
+
 echo "Authenticating to Tower"
 auth_token=`curl -d "{\"username\":\"${TOWER_USERNAME}\", \"password\":\"${TOWER_PASSWORD}\"}" \
                  -H "Content-Type: application/json" \
@@ -7,4 +12,19 @@ auth_token=`curl -d "{\"username\":\"${TOWER_USERNAME}\", \"password\":\"${TOWER
                  https://tower-32.lab.422long.com/api/v2/authtoken/ | jq -r .token`
 
 echo "Authtoken: ${auth_token}"
+
+
+
+echo "Launching job ${JOB_TEMPLATE}"
+
+auth_token=`curl -d "{\"username\":\"${TOWER_USERNAME}\", \"password\":\"${TOWER_PASSWORD}\"}" \
+                 -H "Content-Type: application/json" \
+                 -X GET -k -s \
+                 https://tower-32.lab.422long.com/api/v2/job_templates/25/launch/ | jq -r .token`
+auth_token=`curl -d "{\"username\":\"${TOWER_USERNAME}\", \"password\":\"${TOWER_PASSWORD}\"}" \
+                 -H "Content-Type: application/json" \
+                 -X GET -k -s \
+                 https://tower-32.lab.422long.com/api/v2/job_templates/25/launch/ | jq -r .token`
+
+
 
